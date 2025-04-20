cbd4f5dfa556217cb06d7ceb8ad9c1cdfceb7cc4
albecabrera <albe.cabrera@yahoo.de>
2025-04-21 00:47:06 +0200

    SetTimeout

---
+++ POO/setTimeOut.js
@@ -0,0 +1,7 @@
+// Settimeout
+console.log('Antes');
+setTimeout(() => {
+    console.log('Cualquier código');
+}, 3000);
+console.log('después');
+// En este ejemplo, el código dentro de setTimeout se ejecutará después de 3 segundos.
