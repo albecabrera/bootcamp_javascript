5a67c388c17e96e5a31b5dac8786609620269bcd
albecabrera <albe.cabrera@yahoo.de>
2025-04-24 09:24:03 +0200

    ejercicio bulce for

---
+++ ejercicios_retos.js/ejercicioBucleFor.js
@@ -0,0 +1,21 @@
+// Ejercicio bucle for
+let lista = [1,2,3,5,8];
+
+for (let elemento of lista) {
+    console.log(elemento + 4);
+}
+
+// Otro ejercicio
+let diccionario = ["Ricardo", "Pedro", "Maredo"];
+
+for (let nombres of diccionario) {
+    console.log("Mi nombres es: " + nombres );
+}
+
+let lenguajesProgramacion = ['JS', 'Python', 'Java'];
+
+for (let lenguajes of lenguajesProgramacion) {
+    console.log("Sigo aprendiendo estos lenguajes de programación: " + lenguajes);
+}
+
+
--- ejercicios_retos.js/ejercicio_bucle.js
+++ ejercicios_retos.js/ejercicio_bucle.js
@@ -7,4 +7,12 @@ for (let i = 2; i <= 10; i += 2) {
     console.log(i);
 }
 
+for (let i = 0; i < 5; i++) {
+    console.log('Soy una frase repetitiva'); // repite el rango tantas veces como se pida
+}
  
+// Bucle en una lista
+let lista = ["gato", "perro", "tortuga", "serpiente"];
+for (let i = 0; i < lista.length; i++) {
+    console.log(lista[i] + ' es un animal');
+} 
