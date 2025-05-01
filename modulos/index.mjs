fa9ffc1d1f5a13d1904a43fe1a3871f5a46b2a58
albecabrera <albe.cabrera@yahoo.de>
2025-05-01 22:05:41 +0200

    modulos mjs

---
+++ modulos/index.mjs
@@ -0,0 +1,6 @@
+import { cuadrado, removerDuplicados } from "./modulos.mjs";
+
+cuadrado(2);
+removerDuplicados([1, 2, 3, 3, 3, 4]); 
+ 
+ 
---
+++ modulos/modulos.mjs
@@ -0,0 +1,7 @@
+export function cuadrado (x) {
+console.log(x * x);
+}
+
+export function removerDuplicados (lista) {
+    console.log([...new Set(lista)]);
+}
