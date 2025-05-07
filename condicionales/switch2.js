b0b8537f3b6f19ac3cd607b30987716c91631698
albecabrera <albe.cabrera@yahoo.de>
2025-05-07 22:29:56 +0200

    haciendo ejercicios

---
+++ condicionales/switch2.js
@@ -0,0 +1,14 @@
+// El condicional switch
+let admin3 = 'Emilia'
+let usuario3 = admin
+
+switch (usuario3) {
+    case 'Emilia':
+        console.log('El usuario es admin')
+        break
+    case 'Juan':
+        console.log('El usuario es Juan')
+        break
+    default:
+        console.log('El usuario no es admin')
+}
---
+++ condicionales2.js
@@ -0,0 +1,15 @@
+// Los condicionales
+let admin = 'Emilia'
+let usuario = admin
+
+if (admin == usuario) {
+    console.log('El usuario es admin')
+}
+else {
+    console.log('El usuario no es admin')
+}       
+// El operador ternario
+let admin2 = 'Emilia'
+let usuario2 = admin2
+let resultado = (admin2 == usuario2) ? 'El usuario es admin' : 'El usuario no es admin'
+console.log(resultado)
